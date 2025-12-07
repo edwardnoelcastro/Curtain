f712d8147c654a94ca02620c033171f2d3e45103
jtollossa <jtollossa1@gmail.com>
2025-12-07 16:59:52 -0600

    Update ldr.h

--- ldr.h
+++ ldr.h
@@ -1,3 +1,32 @@
+/*
+ * ldr.h
+ *
+ *  Created on: Nov 30, 2025
+ *      Author: jeremiahtollossa
+ */
+
+#ifndef LDR_H
+#define LDR_H
+
+#include "main.h"
+
+// Thresholds in Volts (3.3V Logic)
+#define OPEN_THRESHOLD  0.9f
+#define CLOSE_THRESHOLD 0.5f
+
+void LDR_Init(ADC_HandleTypeDef *hadc);
+float LDR_ReadVoltage(void);
+
+#endif
+
+
+
+
+
+
+
+//Old ldr.h
+/*
 // ldr.h
 #ifndef LDR_H
 #define LDR_H
@@ -10,3 +39,5 @@ void LDR_Check(void); // periodically called in main loop
 extern uint8_t LDR_Preference; // 1 = ON, 0 = OFF
 
 #endif
+*/
+
