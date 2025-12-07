6cfda339af8ffab017eecd1e1d0c88ac79468888
jtollossa <jtollossa1@gmail.com>
2025-12-07 17:00:44 -0600

    Update motor.h

--- motor.h
+++ motor.h
@@ -1,4 +1,44 @@
-// motor.h
+/*
+ * motor.h
+ *
+ *  Created on: Nov 30, 2025
+ *      Author: jeremiahtollossa
+ */
+
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include "main.h" // Includes STM32 HAL
+
+// Step Configuration
+#define MAX_STEPS 1250
+#define MOTOR_SPEED_DELAY 3 // ~3ms delay between steps
+
+// Motor State Definition
+typedef struct {
+    int currentPosition;
+    int targetPosition;
+} Motor_State_t;
+
+// Function Prototypes
+void Motor_Init(void);
+void Motor_Step(int steps);
+void Motor_Off(void);
+int Motor_GetPosition(void);
+void Motor_SetPosition(int pos);
+
+#endif
+
+
+
+
+
+
+
+
+
+//Old motor.h
+/*// motor.h
 #ifndef MOTOR_H
 #define MOTOR_H
 
@@ -10,3 +50,4 @@ void Motor_Close(void);
 void Motor_Stop(void);
 
 #endif
+*/
